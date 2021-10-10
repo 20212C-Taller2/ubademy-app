cc5e5b919b48c56372310f3a7786f787c6cb9535
Nikita Dyatlov <nexodrone@gmail.com>
2021-10-10 13:56:31 -0300

    Add nav drawer to main activity, font, about fragment

--- app/src/main/java/com/fiuba/ubademy/MainActivity.kt
+++ app/src/main/java/com/fiuba/ubademy/MainActivity.kt
@@ -2,10 +2,32 @@ package com.fiuba.ubademy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.fiuba.ubademy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.mainDrawerLayout
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.mainNavView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.mainNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
