77f433a86beff4fc22df7450424a64d031200eb2
Nikita Dyatlov <nexodrone@gmail.com>
2021-11-11 22:55:16 -0300

    Use autogenerated default_web_client_id and google_api_key

--- app/src/main/java/com/fiuba/ubademy/UbademyApplication.kt
+++ app/src/main/java/com/fiuba/ubademy/UbademyApplication.kt
@@ -8,6 +8,6 @@ class UbademyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        Places.initialize(applicationContext, getString(R.string.ubademy_api_key))
+        Places.initialize(applicationContext, getString(R.string.google_api_key))
     }
 }
--- app/src/main/java/com/fiuba/ubademy/auth/login/LoginFragment.kt
+++ app/src/main/java/com/fiuba/ubademy/auth/login/LoginFragment.kt
@@ -85,7 +85,7 @@ class LoginFragment : Fragment() {
         setupValidators()
 
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.web_client_id))
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
