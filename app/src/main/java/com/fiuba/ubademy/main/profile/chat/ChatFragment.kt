f43cb6af2de0a69cba0957cea754123ee6827226
Nikita Dyatlov <nexodrone@gmail.com>
2021-11-27 16:55:09 -0300

    Add chat UI

--- app/src/main/java/com/fiuba/ubademy/main/profile/chat/ChatFragment.kt
+++ app/src/main/java/com/fiuba/ubademy/main/profile/chat/ChatFragment.kt
@@ -5,13 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.fiuba.ubademy.R
+import com.fiuba.ubademy.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
+
+    private lateinit var viewModel: ChatViewModel
+    private lateinit var binding: FragmentChatBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+    ): View {
+
+        viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_chat,
+            container,
+            false
+        )
+
+        val getUserResponse = ChatFragmentArgs.fromBundle(requireArguments()).getUserResponse
+        viewModel.userDisplayName.value = getUserResponse.displayName
+        viewModel.userId.value = getUserResponse.user.id
+
+        binding.chatSendButton.setOnClickListener {
+            Toast.makeText(context, viewModel.message.value ?: "-", Toast.LENGTH_LONG).show()
+        }
+
+        binding.chatViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 }
