fcf96040a0254b9d8d6baa12b100f746c4bffc8b
Nikita Dyatlov <nexodrone@gmail.com>
2021-11-30 19:30:41 -0300

    Add new subscription data and sort subscriptions

--- app/src/main/java/com/fiuba/ubademy/network/model/Subscription.kt
+++ app/src/main/java/com/fiuba/ubademy/network/model/Subscription.kt
@@ -1,6 +1,11 @@
 package com.fiuba.ubademy.network.model
 
+import com.squareup.moshi.Json
+
 data class Subscription(
     val code: String,
-    val description: String
+    val description: String,
+    val price: Int,
+    @Json(name = "course_limit")
+    val courseLimit: Int
 )
