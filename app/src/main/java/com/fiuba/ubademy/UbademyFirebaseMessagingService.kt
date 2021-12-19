f17f4fc9082e706a56107be1bfc251ea9dacc439
Nikita Dyatlov <nexodrone@gmail.com>
2021-12-19 14:50:07 -0300

    Implement onNewToken

--- app/src/main/java/com/fiuba/ubademy/UbademyFirebaseMessagingService.kt
+++ app/src/main/java/com/fiuba/ubademy/UbademyFirebaseMessagingService.kt
@@ -1,12 +1,27 @@
 package com.fiuba.ubademy
 
+import com.fiuba.ubademy.network.model.UpdateFcmTokenRequest
+import com.fiuba.ubademy.utils.api
+import com.fiuba.ubademy.utils.getSharedPreferencesData
 import com.google.firebase.messaging.FirebaseMessagingService
+import kotlinx.coroutines.*
 import timber.log.Timber
 
 class UbademyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Timber.i("Refreshed FirebaseMessaging token: $token")
 
-        //TODO: sendRegistrationToServer(token)
+        val userId = baseContext.getSharedPreferencesData().id
+        if (userId.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).run {
+                launch {
+                    try {
+                        baseContext.api().updateFcmToken(userId, UpdateFcmTokenRequest(token))
+                    } catch (e: Exception) {
+                        Timber.e(e)
+                    }
+                }
+            }
+        }
     }
 }
